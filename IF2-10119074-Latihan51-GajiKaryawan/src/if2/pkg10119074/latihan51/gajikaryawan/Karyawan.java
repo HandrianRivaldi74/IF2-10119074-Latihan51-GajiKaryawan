1df0a4a93c2b57f5b765fd27072cdf8bf2f27514
HandrianRivaldi74 <72905859+HandrianRivaldi74@users.noreply.github.com>
2020-11-21 15:11:58 +0700

    Add files via upload

---
+++ IF2-10119074-Latihan51-GajiKaryawan/src/if2/pkg10119074/latihan51/gajikaryawan/Karyawan.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if2.pkg10119074.latihan51.gajikaryawan;
+
+/**
+ *
+ * @author Acer
+ * Nama  : HANDRIAN RIVALDI
+ * Kelas : PBOIF2
+ * NIM   : 10119074
+ * Deskrifsi :
+ */
+public class Karyawan {
+   
+    protected String nik;
+    protected String nama;
+    protected String jabatan;
+    protected int golongan;
+
+    public String getNik() {
+        return nik;
+    }
+
+    public void setNik(String nik) {
+        this.nik = nik;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJabatan() {
+        return jabatan;
+    }
+
+    public void setJabatan(String jabatan) {
+        this.jabatan = jabatan;
+    }
+
+    public int getGolongan() {
+        return golongan;
+    }
+
+    public void setGolongan(int golongan) {
+        this.golongan = golongan;
+    }    
+}
